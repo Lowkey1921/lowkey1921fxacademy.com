7a7aa99f4006769760bc3d332b0fea1b7380e729
Lowkey1921 <118089370+Lowkey1921@users.noreply.github.com>
2023-01-14 01:16:21 +0100

    Add files via upload

---
+++ inner2.js
@@ -0,0 +1,8 @@
+function redirect() { window.location="/overview.html"
+  }
+
+function redirecta() { window.location="/fx1.html"
+  }
+
+function redirectb() { window.location="/fx2.html"
+  }
---
+++ timetable.js
@@ -0,0 +1,2 @@
+function redirect() { window.location= "https://chat.whatsapp.com/JkZN6O4ky799XOY1cm4ovY" 
+}
